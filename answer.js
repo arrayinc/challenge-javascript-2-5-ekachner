3312360606908070e60450a23d930783c229040a
ekachner <ekibam@gmail.com>
2019-11-21 19:13:38 -0700

    challenge 5 complete

--- answer.js
+++ answer.js
@@ -1,1 +1,16 @@
 // *** YOUR ANSWER BELOW ***
+function negate(werd){
+    return "un" + werd;
+}
+
+function intensify(werd){
+    return "plus" + werd;
+}
+
+function reinforce(werd){
+    return "double" + werd;
+}
+console.log(negate("cold"));
+console.log(intensify("cold"));
+console.log(reinforce(intensify("cold")));
+console.log(reinforce(intensify(negate("cold"))));
